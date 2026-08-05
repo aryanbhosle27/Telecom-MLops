d521d9e0234c1d51007fd7a217241f96c1a13558
aryanbhosle27 <bhosalearyan471@gmail.com>
2026-08-05 12:58:00 +0530

    Update train.py

--- train.py
+++ train.py
@@ -32,4 +32,4 @@ joblib.dump(model,"telecom_tower_model.pkl")
 metrics={"accuracy":accuracy}
 with open("metrics.json","w") as f:
  json.dump(metrics,f,indent=4)
-print("Training Completed Successfully")
+print("Training Started")
